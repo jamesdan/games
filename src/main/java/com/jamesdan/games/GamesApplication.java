cf1f9893fe48f4a4240a90a4f743e260726bafe6
jamesdanfajardo <jamesdanfajardo@gmail.com>
2025-06-15 00:28:56 +0800

    initial commit - java spring with jdk21

---
+++ src/main/java/com/jamesdan/games/GamesApplication.java
@@ -0,0 +1,15 @@
+package com.jamesdan.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApplication.class, args);
+
+		System.out.println("Hoooooooray!!!");
+	}
+
+}
